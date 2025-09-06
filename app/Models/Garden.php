b0d9cbfe3e090c829d46e3ba67c63261211844b2
IMAM SODIK <imamsodik00@gmail.com>
2025-09-06 15:40:40 +0700

    adf

--- app/Models/Garden.php
+++ app/Models/Garden.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Garden extends Model
 {
@@ -11,4 +12,8 @@ class Garden extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function plants(): HasMany{
+        return $this->hasMany(Plant::class);
+    }
 }
