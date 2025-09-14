297c551f6017d37a2685b226f52a62c647496516
IMAM SODIK <imamsodik00@gmail.com>
2025-09-14 21:41:47 +0700

    adfd

--- resources/views/developers/index.blade.php
+++ resources/views/developers/index.blade.php
@@ -26,7 +26,7 @@
                 <div class="card social-profile">
                     <div class="card-body">
                         <div class="social-img-wrap">
-                            <div class="social-img"><img src="{{asset('dashboard_assets/assets/images/dashboard-3/profile.png')}}" alt="profile">
+                            <div class="social-img"><img src="{{asset('own_assets/images/devs/arif.jpeg')}}" alt="profile">
                             </div>
                             <div class="edit-icon">
                                 <svg>
@@ -36,16 +36,34 @@
                         </div>
                         <div class="social-details">
                             <h5 class="mb-1"><a href="#" style="font-size: 25px">Arif</a></h5><span
-                                class="f-light">9893745877</span>
+                                class="f-light">@rep_arif</span>
                             <ul class="card-social">
                                 <li>
-                                    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" style="font-size: 20px"></i></a>
+                                    <a href="https://www.instagram.com/rep_arif" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
                                 </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-xl-4 col-sm-6 col-xxl-3 col-ed-4 box-col-4">
+                <div class="card social-profile">
+                    <div class="card-body">
+                        <div class="social-img-wrap">
+                            <div class="social-img"><img src="{{asset('own_assets/images/devs/zakwan.jpeg')}}" alt="profile">
+                            </div>
+                            <div class="edit-icon">
+                                <svg>
+                                    <use href="{{asset('dashboard_assets/assets/svg/icon-sprite.svg#profile-check')}}"></use>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="social-details">
+                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Zakwan</a></h5><span
+                                class="f-light">@zakwanhrp</span>
+                            <ul class="card-social">
                                 <li>
-                                    <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
+                                    <a href="https://www.instagram.com/zakwanhrp" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
                                 </li>
                             </ul>
                         </div>
@@ -86,7 +104,7 @@
                 <div class="card social-profile">
                     <div class="card-body">
                         <div class="social-img-wrap">
-                            <div class="social-img"><img src="{{asset('dashboard_assets/assets/images/dashboard-3/profile.png')}}" alt="profile">
+                            <div class="social-img"><img src="{{asset('own_assets/images/devs/moan.jpeg')}}" alt="profile">
                             </div>
                             <div class="edit-icon">
                                 <svg>
@@ -95,17 +113,11 @@
                             </div>
                         </div>
                         <div class="social-details">
-                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Maan</a></h5><span
-                                class="f-light">9893745873</span>
+                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Moan</a></h5><span
+                                class="f-light">@moannsgn_</span>
                             <ul class="card-social">
                                 <li>
-                                    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
+                                    <a href="https://www.instagram.com/moannsgn_" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
                                 </li>
                             </ul>
                         </div>
@@ -116,7 +128,7 @@
                 <div class="card social-profile">
                     <div class="card-body">
                         <div class="social-img-wrap">
-                            <div class="social-img"><img src="{{asset('dashboard_assets/assets/images/dashboard-3/profile.png')}}" alt="profile">
+                            <div class="social-img"><img src="{{asset('own_assets/images/devs/talitha.jpeg')}}" alt="profile">
                             </div>
                             <div class="edit-icon">
                                 <svg>
@@ -125,17 +137,11 @@
                             </div>
                         </div>
                         <div class="social-details">
-                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Talita</a></h5><span
-                                class="f-light">9893745877</span>
+                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Talitha</a></h5><span
+                                class="f-light">@talitharizq</span>
                             <ul class="card-social">
                                 <li>
-                                    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
+                                    <a href="https://www.instagram.com/talitharizq" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
                                 </li>
                             </ul>
                         </div>
@@ -146,7 +152,7 @@
                 <div class="card social-profile">
                     <div class="card-body">
                         <div class="social-img-wrap">
-                            <div class="social-img"><img src="{{asset('dashboard_assets/assets/images/dashboard-3/profile.png')}}" alt="profile">
+                            <div class="social-img"><img src="{{asset('own_assets/images/devs/keysha.jpeg')}}" alt="profile">
                             </div>
                             <div class="edit-icon">
                                 <svg>
@@ -155,17 +161,11 @@
                             </div>
                         </div>
                         <div class="social-details">
-                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Kesya</a></h5><span
-                                class="f-light">9693745877</span>
+                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Keysha</a></h5><span
+                                class="f-light">@keyzhayura</span>
                             <ul class="card-social">
                                 <li>
-                                    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
+                                    <a href="https://www.instagram.com/keyzhayura" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
                                 </li>
                             </ul>
                         </div>
@@ -176,7 +176,7 @@
                 <div class="card social-profile">
                     <div class="card-body">
                         <div class="social-img-wrap">
-                            <div class="social-img"><img src="{{asset('dashboard_assets/assets/images/dashboard-3/profile.png')}}" alt="profile">
+                            <div class="social-img"><img src="{{asset('own_assets/images/devs/rasyid.jpeg')}}" alt="profile">
                             </div>
                             <div class="edit-icon">
                                 <svg>
@@ -185,17 +185,11 @@
                             </div>
                         </div>
                         <div class="social-details">
-                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Rasyd</a></h5><span
-                                class="f-light">9893745477</span>
+                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Rasyid</a></h5><span
+                                class="f-light">@rafieabdullah_</span>
                             <ul class="card-social">
                                 <li>
-                                    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
+                                    <a href="https://www.instagram.com/rafieabdullah_" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
                                 </li>
                             </ul>
                         </div>
