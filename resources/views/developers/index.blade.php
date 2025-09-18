31ae62bc4a16a586689a8a788c0268540d1d9865
IMAM SODIK <imamsodik00@gmail.com>
2025-09-18 20:53:09 +0700

    asdf

--- resources/views/developers/index.blade.php
+++ resources/views/developers/index.blade.php
@@ -74,36 +74,6 @@
                 <div class="card social-profile">
                     <div class="card-body">
                         <div class="social-img-wrap">
-                            <div class="social-img"><img src="{{asset('dashboard_assets/assets/images/dashboard-3/profile.png')}}" alt="profile">
-                            </div>
-                            <div class="edit-icon">
-                                <svg>
-                                    <use href="{{asset('dashboard_assets/assets/svg/icon-sprite.svg#profile-check')}}"></use>
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="social-details">
-                            <h5 class="mb-1"><a href="#" style="font-size: 25px">Fahriza</a></h5><span
-                                class="f-light">9893745897</span>
-                            <ul class="card-social">
-                                <li>
-                                    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" style="font-size: 20px"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" style="font-size: 20px"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-xl-4 col-sm-6 col-xxl-3 col-ed-4 box-col-4">
-                <div class="card social-profile">
-                    <div class="card-body">
-                        <div class="social-img-wrap">
                             <div class="social-img"><img src="{{asset('own_assets/images/devs/moan.jpeg')}}" alt="profile">
                             </div>
                             <div class="edit-icon">
