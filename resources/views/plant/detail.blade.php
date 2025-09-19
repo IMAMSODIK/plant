3463ac20d3172dc239afe8aee5327a5fb6f24686
IMAM SODIK <imamsodik00@gmail.com>
2025-09-19 17:55:38 +0700

    adf

--- resources/views/plant/detail.blade.php
+++ resources/views/plant/detail.blade.php
@@ -69,13 +69,6 @@
                                     </div>
                                 </div>
                             </li>
-                            <li>
-                                <div class="balance-item success">
-                                    <div>
-                                        <button class="btn btn-primary activate-pump">Pump Activate</button>
-                                    </div>
-                                </div>
-                            </li>
                         </ul>
                         <ul>
                             <li>
